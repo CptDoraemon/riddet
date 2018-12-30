e0d058234ba36a5f915f04ad925bbbc7a731fb67
CptDoraemon <yxx0827@gmail.com>
2018-12-30 01:51:48 -0500

    working on texteditor

--- client/src/components/createpost/texteditor.js
+++ client/src/components/createpost/texteditor.js
@@ -27,26 +27,16 @@ class ItemGenerator extends React.Component {
     }
 }
 
-class TextEditor extends React.Component {
+class TeTitle extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             teTitle: 'Title (required)',
-            tePost: 'Text (optional)',
-            bold: false,
-            italic: false,
-            spoiler: false,
-            strikethrough: false,
-            list: false,
-            markdown: false,
-            preview: false
         };
-        this.handleTextareaChange = this.handleTextareaChange.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.teTitleDefault = 'Title (required)';
         this.clearDefault = this.clearDefault.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
-        this.tePostDefault = 'Text (optional)';
-        this.teTitleDefault = 'Title (required)';
-        this.toggleButton = this.toggleButton.bind(this);
     }
     clearDefault(e) {
         let defaultValue = e.target.id === 'teTitle' ? this.teTitleDefault : this.tePostDefault;
@@ -64,36 +54,83 @@ class TextEditor extends React.Component {
             })
         }
     }
-    handleTextareaChange(e) {
+    handleTitleChange(e) {
         let content = e.target.value;
         this.setState({
-            [e.target.id]: content
+            teTitle: content,
         });
-
-        function getActualHeight(el) {
-            el.style.height = '0';
-            let scrollHeight = el.scrollHeight;
-            let paddingTop = window.getComputedStyle(el, null).getPropertyValue('padding-top');
-            let paddingBot = window.getComputedStyle(el, null).getPropertyValue('padding-bottom');
-            paddingTop = parseFloat(paddingTop);
-            paddingBot = parseFloat(paddingBot);
-            let height = scrollHeight - paddingTop - paddingBot;
-            return height;
-        }
-
+        // Adjust Textarea Height
+        // getActualHeight() is at bottom
         let el = document.getElementById(e.target.id);
         let actualHeight = getActualHeight(el);
-        if (e.target.id === 'teTitle') {
-            el.style.height = actualHeight + 'px';
+        el.style.height = actualHeight + 'px';
+    }
+    render() {
+        const handlers = {
+            onFocus: this.clearDefault,
+            onBlur: this.handleBlur
+        };
+        return (
+            <div className='text-editor-title'>
+                        <textarea
+                            className={this.state.teTitle === this.teTitleDefault ? 'text-editor-editor-default-text' : null}
+                            id='teTitle' name='title' value={this.state.teTitle} {...handlers}
+                            onChange={this.handleTitleChange}/>
+            </div>
+        )
+    }
+}
+
+class TePost extends  React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tePost: 'Text (optional)',
+            bold: false,
+            italic: false,
+            spoiler: false,
+            strikethrough: false,
+            markdown: false,
+            preview: false,
+            selectionStart: null,
+            selectionEnd: null
+        };
+        this.tePostDefault = 'Text (optional)';
+        this.handlePostChange = this.handlePostChange.bind(this);
+        this.clearDefault = this.clearDefault.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.toggleButton = this.toggleButton.bind(this);
+        this.shortcutListener = this.shortcutListener.bind(this);
+    }
+    clearDefault(e) {
+        if (e.target.value === this.tePostDefault) {
+            this.setState({
+                tePost: ''
+            })
         }
-        if (e.target.id === 'tePost') {
-            let preview = document.getElementById('tePreview');
-            let previewHeight = getActualHeight(preview);
-            let setHeight = Math.max(actualHeight, previewHeight);
-            el.style.height = setHeight + 'px';
-            preview.style.height = setHeight + 'px';
-            this.renderPreview(content);
+    }
+    handleBlur(e) {
+        if (e.target.value === '') {
+            this.setState({
+                tePost: this.tePostDefault
+            })
         }
+    }
+    handlePostChange(e) {
+        let content = e.target.value;
+        let el = document.getElementById('tePost');
+        this.setState({
+            tePost: content,
+        });
+        // Adjust Textarea Height
+        // getActualHeight() is at bottom
+        let actualHeight = getActualHeight(el);
+        let preview = document.getElementById('tePreview');
+        let previewHeight = getActualHeight(preview);
+        let setHeight = Math.max(actualHeight, previewHeight);
+        el.style.height = setHeight + 'px';
+        preview.style.height = setHeight + 'px';
+        this.renderPreview(content);
     }
     renderPreview(content) {
         let preview = document.getElementById('tePreview');
@@ -102,42 +139,101 @@ class TextEditor extends React.Component {
         while (preview.firstChild) {
             preview.removeChild(preview.firstChild);
         }
-        console.log(content);
         content = JSON.stringify(content);
-        console.log(content);
         content = content.slice(1, content.length - 1);
         content = content.split('\\n');
         content.map((i) => {
             node = document.createElement('p');
-            textnode = i === '' ?  document.createTextNode(' '): document.createTextNode(i);
+            textnode = document.createTextNode(i);
             node.appendChild(textnode);
             preview.appendChild(node);
         });
-        console.log(content);
-
     }
     toggleButton(buttonName) {
         let toggleButton = () => {
             let el = document.getElementById('tePost');
             let start = el.selectionStart;
             let end = el.selectionEnd;
-            let post;
-            let selection;
+            let post = this.state.tePost.slice();
+            let selection = post.slice(start, end);
+            let markupLeading;
+            let markupTrailing;
+            let markupLength; // HALF
+            el.focus();
 
             if (buttonName === 'bold') {
-                if (start === end) {
+                markupLeading = '**';
+                markupTrailing = '**';
+            } else if (buttonName === 'italic') {
+                markupLeading = '*';
+                markupTrailing = '*';
+            } else if (buttonName === 'strikethrough') {
+                markupLeading = '~~';
+                markupTrailing = '~~';
+            } else if (buttonName === 'spoiler') {
+                markupLeading = '>!';
+                markupTrailing = '!<';
+            }
+            markupLength = markupLeading.length;
+            const exactMatch = (selection.slice(0, markupLength) === markupLeading && selection.slice(selection.length - markupLength, selection.length) === markupTrailing);
+
+
+            // CASE 0: BUTTON CLICKED WHEN POST EMPTY
+            if (this.state.tePost === this.tePostDefault) {
+                post = markupLeading + markupTrailing;
+                this.setState({
+                    [buttonName]: true,
+                    tePost: post,
+                    selectionStart: markupLength,
+                    selectionEnd: markupLength,
+                });
+            } // CASE 1: TURN ON STYLE
+            else if (start === end && !this.state[buttonName]) {
+                post = post.slice(0, start) + markupLeading + markupTrailing + post.slice(start, post.length);
+                this.setState({
+                    [buttonName]: true,
+                    tePost: post,
+                    selectionStart: start + markupLength,
+                    selectionEnd: start + markupLength,
+                });
+            } // CASE 2: TURN OFF STYLE
+            else if (start === end && this.state[buttonName]) {
+                const nothingTyped = (post.slice(start - markupLength, start) === markupLeading && post.slice(end, end + markupLength) === markupTrailing);
+                //CASE 2.0 NOTHING TYPED
+                if (nothingTyped) {
+                    post = post.slice(0, start - markupLength) + post.slice(end + markupLength, post.length);
                     this.setState({
-                        [buttonName]: !this.state[buttonName]
+                        [buttonName]: false,
+                        tePost: post,
+                        selectionStart: start,
+                        selectionEnd: start,
                     });
-                } else {
-                    post = this.state.tePost.slice();
-                    selection = this.state.tePost.slice(start, end);
-                    selection = '<b>' + selection + '</b>';
-                    post = post.slice(0, start) + selection + post.slice(end, post.length);
+                } //CASE 2.1 TYPED SOMETHING
+                else {
                     this.setState({
-                        tePost: post
+                        [buttonName]: false,
+                        selectionStart: start + markupLength,
+                        selectionEnd: start + markupLength,
                     });
                 }
+            }// CASE 3: CONVERT SELECTION INTO STYLE
+            else if (start !== end && !this.state[buttonName] && !exactMatch) {
+                post = post.slice(0, start) + markupLeading + selection + markupTrailing + post.slice(end, post.length);
+                this.setState({
+                    [buttonName]: false,
+                    tePost: post,
+                    selectionStart: start,
+                    selectionEnd: end + markupLength * 2,
+                });
+            } // CASE 4: SELECTION UNSTYLE (EXACT MATCH **A-Z**)
+            else if (start !== end && !this.state[buttonName] && exactMatch) {
+                post = post.slice(0, start) + selection.slice(markupLength, selection.length - markupLength) + post.slice(end, post.length);
+                this.setState({
+                    [buttonName]: false,
+                    tePost: post,
+                    selectionStart: start,
+                    selectionEnd: end - markupLength * 2,
+                });
             }
         };
         return toggleButton;
@@ -146,16 +242,51 @@ class TextEditor extends React.Component {
         let togglePreview = () => {
             if(buttonName === 'markdown') {
                 !this.state.preview ? this.setState({markdown: !this.state.markdown}) : this.setState({markdown: true, preview: false});
-                }
+            }
             if(buttonName === 'preview') {
                 !this.state.markdown ? this.setState({preview: !this.state.preview}) : this.setState({preview: true, markdown: false});
             }
-            };
-        return togglePreview;
         };
+        return togglePreview;
+    };
+    shortcutListener(e) {
+        let ctrl = (e.ctrlKey || e.metaKey);
+        if (e.key === 'b' && ctrl) {
+            e.preventDefault();
+            (this.toggleButton('bold'))();
+        }
+        if (e.key === 'i' && ctrl) {
+            e.preventDefault();
+            (this.toggleButton('italic'))();
+        }
+        if (e.key === 'd' && ctrl) {
+            e.preventDefault();
+            (this.toggleButton('strikethrough'))();
+        }
+        if (e.key === 's' && ctrl) {
+            e.preventDefault();
+            (this.toggleButton('spoiler'))();
+        }
+    }
+    componentDidUpdate() {
+        if (this.state.selectionStart || this.state.selectionEnd) {
+            let el = document.getElementById('tePost');
+            el.selectionStart = this.state.selectionStart;
+            el.selectionEnd = this.state.selectionEnd;
+            this.setState({
+                selectionStart: null,
+                selectionEnd: null
+            })
+        }
+    }
+    componentDidMount() {
+        window.addEventListener('keydown', this.shortcutListener);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.shortcutListener);
+    }
     render() {
         const handlers = {
-            onChange: this.handleTextareaChange,
             onFocus: this.clearDefault,
             onBlur: this.handleBlur
         };
@@ -174,36 +305,50 @@ class TextEditor extends React.Component {
         }
 
         return (
+            <div className='text-editor-editor-wrapper'>
+                <div className='text-editor-editor-toolbox-wrapper'>
+                    <div className='text-editor-editor-toolbox-left'>
+                        <ItemGenerator Icon={FiBold} label='bold (Ctrl+B)' toggleButton={this.toggleButton('bold')} isSelected={this.state.bold}/>
+                        <ItemGenerator Icon={FiItalic} label='italic (Ctrl+I)' toggleButton={this.toggleButton('italic')} isSelected={this.state.italic}/>
+                        <ItemGenerator Icon={FiEdit3} label='strikethrough (Ctrl+D)' toggleButton={this.toggleButton('strikethrough')} isSelected={this.state.strikethrough}/>
+                        <ItemGenerator Icon={FiAlertCircle} label='spoiler (Ctrl+S)' toggleButton={this.toggleButton('spoiler')} isSelected={this.state.spoiler}/>
+                        <ItemGenerator Icon={FiImage} label='add image' />
+                    </div>
+                    <div className='text-editor-editor-toolbox-center'>
+                        <ItemGenerator Icon={FiArrowDownRight} label='markdown' toggleButton={this.togglePreview('markdown')} isSelected={this.state.markdown}/>
+                        <ItemGenerator Icon={FiArrowDownLeft} label='preview' toggleButton={this.togglePreview('preview')} isSelected={this.state.preview}/>
+                    </div>
+                    <div className='text-editor-editor-toolbox-right'>
+                    </div>
+                </div>
+                <div className='text-editor-markdown-wrapper'>
+                            <textarea className={markdownClassName}
+                                      onChange={this.handlePostChange}
+                                      id='tePost' name='post' value={this.state.tePost} {...handlers}/>
+                    <div className={previewClassName} id='tePreview'>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class TextEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
             <div className='text-editor-wrapper'>
                 <form>
-                    <div className='text-editor-title'>
-                        <textarea className={this.state.teTitle === this.teTitleDefault ? 'text-editor-editor-default-text' : null}
-                                  id='teTitle' name='title' value={this.state.teTitle} {...handlers}/>
-                    </div>
-                    <div className='text-editor-editor-wrapper'>
-                        <div className='text-editor-editor-toolbox-wrapper'>
-                            <div className='text-editor-editor-toolbox-left'>
-                                <ItemGenerator Icon={FiBold} label='bold' toggleButton={this.toggleButton('bold')} isSelected={this.state.bold}/>
-                                <ItemGenerator Icon={FiItalic} label='italic' toggleButton={this.toggleButton('italic')} isSelected={this.state.italic}/>
-                                <ItemGenerator Icon={FiEdit3} label='strikethrough' toggleButton={this.toggleButton('strikethrough')} isSelected={this.state.del}/>
-                                <ItemGenerator Icon={FiAlertCircle} label='spoiler' toggleButton={this.toggleButton('spoiler')} isSelected={this.state.spoiler}/>
-                                <ItemGenerator Icon={FiImage} label='add image' />
-                            </div>
-                            <div className='text-editor-editor-toolbox-center'>
-                                <ItemGenerator Icon={FiArrowDownRight} label='markdown' toggleButton={this.togglePreview('markdown')} isSelected={this.state.markdown}/>
-                                <ItemGenerator Icon={FiArrowDownLeft} label='preview' toggleButton={this.togglePreview('preview')} isSelected={this.state.preview}/>
-                            </div>
-                            <div className='text-editor-editor-toolbox-right'>
-                            </div>
-                        </div>
-                        <div className='text-editor-markdown-wrapper'>
-                            <textarea className={markdownClassName}
-                                id='tePost' name='post' value={this.state.tePost} {...handlers}/>
-                            <div className={previewClassName} id='tePreview'>
 
-                            </div>
-                        </div>
-                    </div>
+                    <TeTitle />
+                    <TePost />
+
                     <div className='text-editor-button'>
                         <button style={{backgroundColor: this.props.themeColor[0]}}>Post</button>
                     </div>
@@ -211,6 +356,18 @@ class TextEditor extends React.Component {
             </div>
         )
     }
+}
+
+// function for handleTitleChange & handlePostChange
+function getActualHeight(el) {
+    el.style.height = '0';
+    let scrollHeight = el.scrollHeight;
+    let paddingTop = window.getComputedStyle(el, null).getPropertyValue('padding-top');
+    let paddingBot = window.getComputedStyle(el, null).getPropertyValue('padding-bottom');
+    paddingTop = parseFloat(paddingTop);
+    paddingBot = parseFloat(paddingBot);
+    let height = scrollHeight - paddingTop - paddingBot;
+    return height;
 }
 
 export { TextEditor };
