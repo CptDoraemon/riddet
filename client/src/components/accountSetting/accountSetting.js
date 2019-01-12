a010cf4520b29a5e9339d150a16073a5ca603096
CptDoraemon <yxx0827@gmail.com>
2019-01-12 04:22:26 -0500

    working on setting

--- client/src/components/accountSetting/accountSetting.js
+++ client/src/components/accountSetting/accountSetting.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './accountSetting.css';
 import { HeaderFixedLogo } from "../header";
+import { MdChevronRight } from "react-icons/md";
 
 class AccountSettingHeadbar extends React.Component {
     render() {
@@ -37,6 +38,50 @@ class AccountSettingHeadbar extends React.Component {
         )
     }
 }
+
+function PostEntry (props) {
+    const wrapperClassName = props.isActive ? 'post-entry-wrapper-active' : 'post-entry-wrapper-inactive';
+    return (
+        <div className={wrapperClassName}>
+            <div className='post-entry-info'>Posted by xxxxx date 22-22-22</div>
+            <div className='post-entry-title'>I'm title</div>
+            <div className='post-entry-content'>I'm content</div>
+        </div>
+        )
+}
+function CommentEntry (props) {
+
+}
+class Section extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDropDown: true
+        };
+        this.handleDropdown = this.handleDropdown.bind(this);
+    }
+    handleDropdown() {
+        this.setState({isDropDown: !this.state.isDropDown});
+    }
+    render() {
+        return (
+            <div className='account-setting-section-wrapper'>
+                <div className={this.state.isDropDown ? 'account-setting-section-title-active': 'account-setting-section-title-inactive'}
+                     onClick={this.handleDropdown}>
+                    <span> {this.props.sectionName} </span>
+                    <div className={this.state.isDropDown ? 'account-setting-section-title-icon-active' : 'account-setting-section-title-icon-inactive'} >
+                        <MdChevronRight size='20px' />
+                    </div>
+                </div>
+                <PostEntry isActive={this.state.isDropDown}/>
+                <PostEntry isActive={this.state.isDropDown}/>
+                <PostEntry isActive={this.state.isDropDown}/>
+                <PostEntry isActive={this.state.isDropDown}/>
+                <PostEntry isActive={this.state.isDropDown}/>
+            </div>
+        )
+    }
+}
 class AccountSetting extends React.Component {
     render() {
         return (
@@ -48,10 +93,12 @@ class AccountSetting extends React.Component {
                     <div className='account-setting-content-wrapper'>
                         {/*<AvatarSetting />*/}
                         {/*<ChangePassword />*/}
-                        {/*<SavedPosts />*/}
-                        {/*<SavedComments />*/}
-                        {/*<HiddenPosts />*/}
-                        {/*<HiddenComments />*/}
+                        <Section sectionName='my posts' type='post'/>
+                        <Section sectionName='my comments' type='comment'/>
+                        <Section sectionName='saved posts' type='post'/>
+                        <Section sectionName='saved comments' type='comment'/>
+                        <Section sectionName='hidden posts' type='post'/>
+                        <Section sectionName='hidden comments' type='comment'/>
                     </div>
 
                 </div>
